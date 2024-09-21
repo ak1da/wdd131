56e8f31a0af535abde1cf2b6af26e87673830cae
Ak1da <mrak1da2020@gmail.com>
2024-09-21 22:31:55 +0100

    Week one

---
+++ styles/scripts/getdates.js
@@ -0,0 +1,13 @@
+let short = document.querySelector("#short");
+console.log(short);
+
+
+
+const today = new Date();
+
+short.innerHTML = `<span class="highlight">${new Intl.DateTimeFormat(
+	"en-US",
+	{
+		dateStyle: "short"
+	}
+).format(today)}</span>`;
